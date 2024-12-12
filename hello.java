8f674d534c085292b1bbc2950e75891e20c296d2
1U23CA038GOKULS <139437434+1U23CA038GOKULS@users.noreply.github.com>
2024-12-12 09:54:09 +0000

    hello

---
+++ hello.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class hello
+{
+    public static void main (String args[])
+    {
+        System.out.println("Enter the number");
+        Scanner s1= new Scanner(System.in);
+        int G=s1.nextInt();
+        System.out.println("The value is : " + G);
+
+    }
+}
